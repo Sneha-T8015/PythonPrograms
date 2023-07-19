95be911e217ea9d3daa01f134feb5deb98135337
Sneha T <106481080+Sneha-T8015@users.noreply.github.com>
2023-07-19 22:11:43 +0530

    Create SumColValues.py

---
+++ SumColValues.py
@@ -0,0 +1,13 @@
+import numpy as np
+def solSum(matrix):
+  if not matrix:
+    return -1
+  np_m=np.array(matrix)
+  a=np.sum(np_m,axis=0)
+  return a.tolist()
+m,n=list(map(int,input().split()))
+matrix=[]
+for i in range(m):
+  row=list(map(int,input().split()))
+  matrix.append(row)
+solSum(matrix)
